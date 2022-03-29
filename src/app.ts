0db8bdd63f71509216a5e3e1a632586fafae4c7b
Mehdi Noroozi <mehdi.noroozi@qhrtech.com>
2022-03-29 15:03:08 -0600

    modify app.js

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,27 @@ require('./tracer.ts')('app-services'); //this should be the first line
 import express from 'express'
 
 const app = express();
+const PORT = process.env.PORT || "3200";
 
-app.get('/', (req, res) =>{
-    res.send('Hello');
-});
-
-app.listen(3200, () => console.log('Server running'));
+async function sleep(ms : number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  //index endpoint
+  app.get('/', async(req, res) => {
+    const sleepTime = Math.random()*100;
+    await sleep(sleepTime);
+    res.status(200).send('Hello World ');
+  });
+  
+  //another endpoint
+  app.get('/api', async function (req, res) {
+    // send back Medeo message
+    res.type('json')
+    res.send(JSON.stringify({medeo:"skin"}))
+  })
+  
+  
+  app.listen(parseInt(PORT, 10), () => {
+    console.log(`Listening for requests on http://localhost:${PORT}`);
+  });
